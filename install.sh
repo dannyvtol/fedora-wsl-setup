bc9aff3664e32eba4217b69e68d7c2010cc34f88
Danny van Tol <19685217+dannyvtol@users.noreply.github.com>
2026-08-23 16:40:34 +0200

    Use HTTPS for git clone

--- install.sh
+++ install.sh
@@ -31,7 +31,7 @@ git config --global user.name $GIT_USERNAME
 git config --global user.email $GIT_EMAIL
 
 echo "Cloning repository"
-git clone git@github.com:dannyvtol/fedora-wsl-setup.git
+git clone https://github.com/dannyvtol/fedora-wsl-setup.git
 
 # Install Brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
