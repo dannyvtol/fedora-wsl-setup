3c8a624d77f56cabd63d1362703c0a369c79c5ce
Danny van Tol <danny.van.tol@outlook.com>
2026-08-25 21:32:11 +0200

    Install mattpocock/skill

--- scripts/claude/install.sh
+++ scripts/claude/install.sh
@@ -13,3 +13,5 @@ brew install "${BREW_PACKAGES[@]}" -y
 cp $SCRIPT_DIR/settings.json $HOME/.claude/settings.json
 cp $SCRIPT_DIR/rules/*.md $HOME/.claude/rules/
 cp -r $SCRIPT_DIR/skills $HOME/.claude/skills
+
+npx skills@latest add mattpocock/skills --global -a claude-code
